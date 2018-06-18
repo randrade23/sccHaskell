b46e8a27598156fb096caef5fed5add488239bdb
Rui Andrade <ruiandrade@outlook.com>
2018-06-18 11:10:29 +0100

    Added Tree example.

---
+++ test/Tree/Tree.hs
@@ -0,0 +1,7 @@
+module Tree where
+
+data Tree a = Node a (Tree a) (Tree a) | Leaf
+
+depth :: Tree Int -> Int
+depth Leaf = 0
+depth (Node _ a b) = 1 + (if (depth a) > (depth b) then depth a else depth b)
